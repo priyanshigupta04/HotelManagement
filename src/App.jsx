3e5e9ca276fced35ec783e96eba0948b78d0c277
Priyanshi <prigupta124@gmail.com>
2025-07-18 19:58:22 +0530

    Pages UI done

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Footer from './components/Footer.jsx';
+import AllRooms from './pages/AllRooms.jsx';
+import RoomDetails from './pages/RoomDetails.jsx';
+import MyBookings from './pages/MyBookings.jsx';
+
+const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path ='/' element={<Home/>}/>
+          <Route path='/rooms' element={<AllRooms/>} />
+          <Route path='/rooms/:id' element={<RoomDetails/>} />
+          <Route path='/my-bookings' element={<MyBookings/>} />
+
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/featuredDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
+import Newslatter from '../components/Newslatter'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <FeaturedDestination/>
+        <ExclusiveOffers/>
+        <Testimonial/>
+        <Newslatter/>
+    </>
+  )
+}
+
+export default Home
